1b33ca2d8d44ecb5dbe894c5b53e2ce96f18001e
Kevin Pham <kevin.v.pham@hotmail.com>
2014-11-04 19:49:42 -0800

    Map annotations

--- MapReminder/ViewController.swift
+++ MapReminder/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
@@ -19,17 +19,46 @@ class ViewController: UIViewController {
         // Long Press Gesture Recognizer on Map View
         let longPress = UILongPressGestureRecognizer(target: self, action: "didLongPressMap:")
         mapView.addGestureRecognizer(longPress)
+        
+        self.mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
+    
+    // MARK: - MAP KIT
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        // Configure Map Annotations
+        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "ANNOTATION")
+        annotationView.animatesDrop = true
+        annotationView.canShowCallout = true
+        
+        let addButton = UIButton.buttonWithType(UIButtonType.ContactAdd) as UIButton
+        annotationView.rightCalloutAccessoryView = addButton
+        
+        return annotationView
+    }
 
     // MARK: - GESTURE RECOGNIZER
     
     func didLongPressMap(sender: UILongPressGestureRecognizer) {
-        println("Map Long Pressed.")
+        //println("Map Long Pressed.")
+        
+        // Placing Map Pin on Map
+        if sender.state == UIGestureRecognizerState.Began {
+            let touchPoint = sender.locationInView(self.mapView)
+            let touchCoordinate = self.mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+            println("\(touchCoordinate.latitude) \(touchCoordinate.longitude)")
+            
+            // Creating Map Annotation
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = touchCoordinate
+            annotation.title = "Add Reminder"
+            self.mapView.addAnnotation(annotation)
+        }
     }
 
 }
