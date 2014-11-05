d1d2d3d5bfead6856caed8a89d274dd0bd4b9d16
Kevin Pham <kevin.v.pham@hotmail.com>
2014-11-04 17:36:42 -0800

    initial commit, tab bar controller

---
+++ MapReminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapReminder
+//
+//  Created by Kevin Pham on 11/4/14.
+//  Copyright (c) 2014 Kevin Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
